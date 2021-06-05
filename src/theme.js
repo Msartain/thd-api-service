dce473008e409ca4f69f58464d426814fc5580f6
Matt Sartain <matthewsartain@me.com>
2021-06-05 15:43:06 -0500

    adds Mui theme overrides

---
+++ src/theme.js
@@ -0,0 +1,33 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+export const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#F96302'
+        },
+        secondary: {
+            main: '#fff'
+        }
+    },
+    overrides: {
+        MuiButton: {
+            root: {
+                color: '#F96302',
+                backgroundColor: '#fff',
+                borderRadius: 0,
+                border: '2px solid #F96302',
+                height: 40,
+                boxShadow: 'none',
+                padding: 5,
+                fontSize: 15,
+                fontWeight: 700,
+                minWidth: 150,
+                transition: '.5s ease-in-out',
+                '&:hover': { 
+                    backgroundColor: '#F96302',
+                    color: '#fff',
+                },
+            }
+        }
+    }
+});
